33ec24bdfba7c94af1eecffff819eb8545239421
Renato <renatoyufra@gmail.com>
2021-02-07 22:43:12 -0500

    add custom _app

---
+++ components/Layout/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from 'components/Navbar/Navbar'
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+      <footer>this is de footer</footer>
+    </div>
+  )
+}
+
+export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,6 @@ const HomePage = () => {
 
   return (
     <div>
-      <Navbar />
       <div>Platzi and Next.js!</div>
       {productList.map((product) => (
         <div>{product.image}</div>

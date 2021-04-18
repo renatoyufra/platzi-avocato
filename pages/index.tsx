e86c8cf82daea0729a75503a9372155a6683ca63
Renato <renatoyufra@gmail.com>
2021-04-18 18:35:46 -0500

    change default BACKEND_URL

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import ProductList from '@components/Products/ProductList'
 import React, { useEffect, useState } from 'react'
 
 export const getServerSideProps = async () => {
-  const BASE_URL = process.env.BACKEND_URL || process.env.VERCEL_URL
+  const BASE_URL =
+    process.env.BACKEND_URL || 'https://platzi-avocato.vercel.app/'
   const response = await fetch(`${BASE_URL}api/avo`)
   const { data: productList }: TAPIAvoResponse = await response.json()
   return {

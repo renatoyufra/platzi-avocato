a9fb29a613507c2ddabf93c011521a9971116f76
Renato <renatoyufra@gmail.com>
2021-04-18 23:47:26 -0500

    refactoring page yes-or-no

--- pages/yes-or-no/index.tsx
+++ pages/yes-or-no/index.tsx
@@ -1,47 +1,56 @@
 import Link from 'next/link'
 import { useState, useEffect } from 'react'
 
-export const getServerSideProps = async () => {
+type YesOrNoApiResponse = {
+  data: boolean
+}
+
+const fetchResult = async () => {
   const BASE_URL =
     process.env.BACKEND_URL || 'https://platzi-avocato.vercel.app/'
 
   const res = await fetch(`${BASE_URL}api/question`)
-  const { data: answer } = await res.json()
+  const { data }: YesOrNoApiResponse = await res.json()
+
+  return data
+}
+export const getServerSideProps = async () => {
+  const answer = await fetchResult()
+
   return {
     props: { answer },
   }
 }
 
-const YesNo = ({ answer }: { answer: TAPIQuestionResponse }) => {
-  const [retry, setRetry] = useState(null)
-  const [newAnswer, setNewAnswer] = useState(null)
-
-  const BASE_URL =
-    process.env.BACKEND_URL || 'https://platzi-avocato.vercel.app/'
-
-  const tryAgainHandle = () => {
-    setRetry(true)
-  }
+const YesNo = ({ answer }: { answer: boolean }) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [newAnswer, setNewAnswer] = useState(answer)
+  const [triggerCount, setTriggerCount] = useState(0)
 
   useEffect(() => {
-    const res = fetch(`${BASE_URL}api/question`)
-      .then((response) => response.json())
-      .then((data) => {
-        setNewAnswer(data)
-        console.log('data', data)
-      })
-  }, [retry])
+    setIsLoading(true)
+    fetchResult().then((answer) => {
+      setNewAnswer(answer)
+      setIsLoading(false)
+    })
+  }, [triggerCount])
+
+  const tryAgainHandle = async () => {
+    setTriggerCount(triggerCount + 1)
+  }
+
   return (
     <div className="flex flex-col justify-between mx-auto items-center">
       <div className="font-bold text-8xl mb-6 text-green-500">
-        {retry === null ? (answer ? 'YES' : 'NO') : newAnswer ? 'YES' : 'NO'}
+        {newAnswer ? 'YES' : 'NO '}
       </div>
       <div className="w-full flex flex-col items-center space-y-2">
         <button
           className="flex-none bg-green-400 font-semibold text-white py-2 px-3 rounded-md"
-          onClick={() => tryAgainHandle}
+          onClick={tryAgainHandle}
+          disabled={isLoading}
         >
-          Intentar de nuevo
+          {isLoading ? 'Cargando' : 'Intentar de nuevo'}
         </button>
         <Link href="/">
           <a className="text-sm border border-black py-2 px-3 rounded-md">
